dca568275b71508d76f4789fdf82977fd091dbf0
Aleksandr Butorov <b.ap@mailfence.com>
2026-01-04 16:26:54 +0300

    start

---
+++ astro.config.mjs
@@ -0,0 +1,26 @@
+// astro.config.mjs
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  server: {
+    host: "0.0.0.0",
+    port: 4321,
+  },
+
+  integrations: [react()],
+
+  vite: {
+    plugins: [tailwindcss()],
+    server: {
+      watch: {
+        usePolling: true,
+        interval: 1000,
+      },
+      hmr: {
+        clientPort: 4321,
+      },
+    },
+  },
+});
