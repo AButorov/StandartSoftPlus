35f056842e272c04ca92d56eec314879b0d997ed
AButorov <butorov.ap@gmail.com>
2026-01-22 21:43:44 +0000

    fix local run problem

--- astro.config.dev.mjs
+++ astro.config.dev.mjs
@@ -4,7 +4,7 @@ import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  site: "https://abutorov.github.io",
+  site: "http://localhost:4321",
   base: "/",
 
   server: {
